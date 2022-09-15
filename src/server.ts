4df5ec0529c05ecd5f4199387fa34056ad9dfe03
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-09-15 12:50:40 -0300

    Banco de dados com sqlite e prisma

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,23 @@ import express from "express"
 const app = express()
 
 
-app.get('/ads', (req, res) => {
-    return res.json([
+app.get('/games', (req, res) => {
+    return res.status(200).json([])
+})
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json([])
+})
+
+app.get('games/:id/ads', (req, res) => {
+    return res.status(200).json([
         {id: 1, name: "Anúncio 1"},
         {id: 2, name: "Anúncio 2"}
     ])
+})
+
+app.get('ads/:id/discord', (req, res) => {
+    return res.status(200).json([])
 })
 
 app.listen(3333)
